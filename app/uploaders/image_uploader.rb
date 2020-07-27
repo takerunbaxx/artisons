dcf1f069d03d8a1f7ec295bea4878215e7c839ad
takeru <tmrevonaitomea@yahoo.co.jp>
2020-07-27 16:53:54 +0000

    artisons-second

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,23 +20,20 @@ process resize_to_limit: [400, 400]
 
 
   # Choose what kind of storage to use for this uploader:
-if Rails.env.production?
-  storage :fog
-else
+
   storage :file
-end
 
 
-if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],  # S3に設定したリージョン。
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-    end
+
+#if Rails.env.production?
+    #CarrierWave.configure do |config|
+     # config.fog_credentials = {
+        #:provider              => 'AWS',
+        #:region                => ENV['S3_REGION'],  # S3に設定したリージョン。
+        #:aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        #:aws_secret_access_key => ENV['S3_SECRET_KEY'] }
+        #config.fog_directory     =  ENV['S3_BUCKET']
+  #end
     
     
   # Override the directory where uploaded files will be stored.
